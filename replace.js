f299d7b4051c21d0c0f95b2c50d860d605c15c88
japantoothpick <oyayubimax@gmail.com>
2015-11-28 23:59:16 +0900

    Update replace.js

--- replace.js
+++ replace.js
@@ -1,6 +1,7 @@
 $(function() {
     $(".replace,.ninja-recommend-article-title,.ninja-recommend-article-text-box").each(function() {
         var e = $(this).html();
-        $(this).html(e.replace(/ロリ/g, "ロ*").replace(/レイプ/g, "レ*プ").replace(/痴漢/g, "痴*").replace(/近親相姦/g, "近*姦").replace(/援交/g, "援*").replace(/催眠/g, "催*").replace(/調教/g, "調*").replace(/盗撮/g, "盗*").replace(/未成年/g, "未*年").replace(/拘束/g, "拘*").replace(/輪姦/g, "輪*").replace(/拷問/g, "拷*").replace(/凌辱/g, "凌*").replace(/JK/g, "J*").replace(/女子高生/g, "女子*生").replace(/監禁/g, "監*").replace(/JC/g, "J*").replace(/流出/g, "流*").replace(/JC/g, "J*").replace(/JS/g, "J*").replace(/監禁/g, "監*").replace(/獣/g, "*").replace(/陵辱/g, "陵*").replace(/凌辱/g, "凌*").replace(/未成年/g, "*").replace(/ジュニアアイドル/g, "アイドル").replace(/無理矢理/g, "無*矢理").replace(/泥酔/g, "*酔"))
+        $(this).html(e.replace(/ロリ/g, "ロ*").replace(/レイプ/g, "レ*プ").replace(/痴漢/g, "痴*").replace(/近親相姦/g, "近*姦").replace(/援交/g, "援*").replace(/催眠/g, "催*").replace(/調教/g, "調*").replace(/盗撮/g, "盗*").replace(/未成年/g, "未*年").replace(/拘束/g, "拘*").replace(/輪姦/g, "輪*").replace(/拷問/g, "拷*").replace(/凌辱/g, "凌*").replace(/JK/g, "J*").replace(/女子高生/g, "女子*生").replace(/監禁/g, "監*").replace(/JC/g, "J*").replace(/流出/g, "流*").replace(/JC/g, "J*").replace(/JS/g, "J*").replace(/監禁/g, "監*").replace(/獣/g, "*").replace(/陵辱/g, "陵*").replace(/凌辱/g, "凌*").replace(/未成年/g, "*").replace(/ジュニアアイドル/g, "アイドル").replace(/無理矢理/g, "無*矢理").replace(/泥酔/g, "*酔").replace(/女子校生/g, "女子*生")
+        )
     })
 });
