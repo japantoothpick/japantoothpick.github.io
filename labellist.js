da43fb9ba2ae9b2907660048325eaf338c31b178
japantoothpick <oyayubimax@gmail.com>
2016-03-17 16:09:12 +0900

    Update labellist.js

--- labellist.js
+++ labellist.js
@@ -243,7 +243,7 @@ function displayToc2() {
     while (b < postTitle.length) {
         temp1 = postLabels[b];
         document.write("<p/>");
-        document.write('<p><a href="/search/label/' + temp1 + '">' + temp1 + "</a></p><ol>");
+        document.write('<p><a href="/search/label/' + temp1 + '">' + temp1 + "</a></p><ul>");
         firsti = a;
         do {
             document.write("<li>");
@@ -252,7 +252,7 @@ function displayToc2() {
             a = a + 1
         } while (postLabels[a] == temp1);
         b = a;
-        document.write("</ol>");
+        document.write("</ul>");
         sortPosts2(firsti, a);
         if (b > postTitle.length) {
             break
